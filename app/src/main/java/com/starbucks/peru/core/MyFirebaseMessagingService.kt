b65bf7515b6cdb542a4dade94b6f43c55354bc8f
Luis Lopez <prov284@franquiciasperu.com>
2023-02-22 18:41:43 -0500

    feat: skd mkcloud

--- app/src/main/java/com/starbucks/peru/core/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/starbucks/peru/core/MyFirebaseMessagingService.kt
@@ -2,22 +2,34 @@ package com.starbucks.peru.core
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.salesforce.marketingcloud.messages.push.PushMessageManager
+import com.salesforce.marketingcloud.sfmcsdk.SFMCSdk
 import com.starbucks.peru.core.utils.LogUtils
 
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+    override fun onMessageReceived(message: RemoteMessage) {
         //remoteMessage.notification!!.body?.let { LogUtils().v("FIREBASE", it) }
-        LogUtils().v("onMessageReceived", "$remoteMessage")
+        LogUtils().v("onMessageReceived", "$message")
+        if (PushMessageManager.isMarketingCloudPush(message)) {
+            SFMCSdk.requestSdk { sdk ->
+                sdk.mp {
+                    it.pushMessageManager.handleMessage(message)
+                }
+            }
+        } else {
+            // Not a push from the Marketing Cloud.  Handle manually.
+        }
     }
 
     override fun onNewToken(token: String) {
         LogUtils().v("FIREBASE", "Refreshed token: $token")
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
+        SFMCSdk.requestSdk { sdk ->
+            sdk.mp {
+                it.pushMessageManager.setPushToken(token)
+            }
+        }
 
     }
 }
